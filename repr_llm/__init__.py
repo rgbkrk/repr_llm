b23a26c1b0e2165bcf42f61c30ac5d011bdf08ab
Kyle Kelley <rgbkrk@gmail.com>
2023-07-04 15:47:48 -0700

    update version

--- repr_llm/__init__.py
+++ repr_llm/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Kyle Kelley"""
 __email__ = 'rgbkrk@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .display import PlainForLLM
 from .formatter import LLMFormatter, register_llm_formatter
